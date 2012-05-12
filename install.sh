869ee65a548ff73e3180f26e806bef84a5b25a08
Zhenya Ast <astzhe@gmail.com>
2012-05-13 02:18:16 +0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "Copying bin/guws to /usr/bin/guws"
+cp bin/guws /usr/bin/guws
+echo "Copying bin/flm to /usr/bin/flm"
+cp bin/flm /usr/bin/flm
+echo "Copying bin/checkep to /usr/bin/checkep"
+cp bin/checkep /usr/bin/checkep
+echo "Copying bin/watch to /usr/bin/watch"
+cp bin/watch /usr/bin/watch
+echo "Copying bash_completion.d/watch to /etc/bash_completion.d/watch"
+cp bash_completion.d/watch /etc/bash_completion.d/watch
